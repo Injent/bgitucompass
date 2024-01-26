8f9121efe807988736d872416e4e57f3dc478277
Injent <injent@vk.com>
2024-01-26 20:26:38 +0300

    u

--- site/src/jsMain/kotlin/ru/bgitucompass/components/sections/Footer.kt
+++ site/src/jsMain/kotlin/ru/bgitucompass/components/sections/Footer.kt
@@ -2,9 +2,9 @@ package ru.bgitucompass.components.sections
 
 import androidx.compose.runtime.Composable
 import com.varabyte.kobweb.compose.css.FontWeight
-import com.varabyte.kobweb.compose.css.JustifyItems
-import com.varabyte.kobweb.compose.foundation.layout.Box
+import com.varabyte.kobweb.compose.foundation.layout.Arrangement
 import com.varabyte.kobweb.compose.foundation.layout.Column
+import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.compose.ui.toAttrs
@@ -12,7 +12,6 @@ import com.varabyte.kobweb.silk.components.layout.SimpleGrid
 import com.varabyte.kobweb.silk.components.layout.numColumns
 import com.varabyte.kobweb.silk.components.style.toAttrs
 import com.varabyte.kobweb.silk.components.style.toModifier
-import org.jetbrains.compose.web.css.AlignItems
 import org.jetbrains.compose.web.css.px
 import org.jetbrains.compose.web.dom.A
 import org.jetbrains.compose.web.dom.P
@@ -32,45 +31,35 @@ fun Footer() {
             .padding(32.px)
             .minHeight(150.px)
     ) {
-        val linkContainerModifier = Modifier.fillMaxWidth().alignItems(AlignItems.Center).justifyItems(JustifyItems.Center)
-
-        Box(linkContainerModifier) {
-            Column {
-                P(
-                    BodyTextStyle.toModifier().fontWeight(FontWeight.SemiBold).toAttrs()
-                ) {
-                    Text("Связь с разработчиками")
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            P(
+                BodyTextStyle.toModifier().fontWeight(FontWeight.SemiBold).toAttrs()
+            ) {
+                Text("Связь с разработчиками")
+            }
+            A(
+                attrs = FooterLinkTextStyle
+                    .toAttrs(),
+                href = "https://t.me/koespe"
+            ) {
+                Text("Пудов Кирилл")
+                Span(BodyTextStyle.toAttrs()) {
+                    Text(" — Cерверная часть и Telegram-бот")
                 }
-                A(
-                    attrs = FooterLinkTextStyle
-                        .toAttrs(),
-                    href = "https://t.me/koespe"
-                ) {
-                    Text("Пудов Кирилл")
-                    Span(BodyTextStyle.toAttrs()) {
-                        Text(" — Cерверная часть и Telegram-бот")
-                    }
-                }
-                A(
-                    attrs = FooterLinkTextStyle.toAttrs(),
-                    href = "https://t.me/Injent"
-                ) {
-                    Text("Веревкин Елисей")
-                    Span(BodyTextStyle.toAttrs()) {
-                        Text(" — Android приложение")
-                    }
+            }
+            A(
+                attrs = FooterLinkTextStyle.toAttrs(),
+                href = "https://t.me/Injent"
+            ) {
+                Text("Веревкин Елисей")
+                Span(BodyTextStyle.toAttrs()) {
+                    Text(" — Android приложение")
                 }
             }
         }
-
-//        Box(linkContainerModifier) {
-//            A(
-//                attrs = FooterLinkTextStyle.toAttrs(),
-//                href = "https://bgitu-compass.ru/userAgreement/"
-//            ) {
-//                Text("Пользовательское соглашение")
-//            }
-//        }
-
     }
 }
